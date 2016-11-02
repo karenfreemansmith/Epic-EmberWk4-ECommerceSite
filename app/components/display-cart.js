7d9c5d005aff04bc26c3762ba79441f0388da3d1
Addison Nishijima and Karen Freeman-Smith <addison+karen@>
2016-11-02 11:07:06 -0700

    add totals to cart items

--- app/components/display-cart.js
+++ app/components/display-cart.js
@@ -2,4 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   shoppingCart: Ember.inject.service(),
+  total: Ember.computed(function() {
+    var t=0;
+    this.get('shoppingCart.items').forEach(function(item) {
+      t+=parseFloat(item.price);
+    });
+    return t;
+  })
 });
+
+//total: Ember.computed('item.price').forEach(function(item) {
