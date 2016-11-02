61a097936a53a47420f87aacd54a188d0f76795c
Addison Nishijima and Karen Freeman-Smith <addison+karen@>
2016-11-02 14:18:16 -0700

    put multiple images into slider

--- app/routes/store.js
+++ app/routes/store.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     // var key = config.myApiKey;
-    var url = 'https://openapi.etsy.com/v2/listings/trending?limit=50&offset=0&api_key=pl2m1ozgt5dd3v33i7lgeo6u&category=69201265&includes=Images';
+    var url = 'https://openapi.etsy.com/v2/listings/active?limit=25&offset=0&api_key=pl2m1ozgt5dd3v33i7lgeo6u&includes=Images';
     return Ember.$.getJSON(url).then(function(responseJSON) {
       console.log(responseJSON.results);
       return responseJSON.results;
